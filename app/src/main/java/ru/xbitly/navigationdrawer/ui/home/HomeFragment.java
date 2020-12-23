e99b6267bea5b8aaac3244f31f4a8d661aacc227
xBitly <xBitly@mail.ru>
2020-12-23 18:25:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/xbitly/navigationdrawer/ui/home/HomeFragment.java
@@ -0,0 +1,23 @@
+package ru.xbitly.navigationdrawer.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import ru.xbitly.navigationdrawer.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        final TextView textView = root.findViewById(R.id.text_slideshow);
+        textView.setText("Home");
+        return root;
+    }
+}
